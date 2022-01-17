a1e022e79ab21b56dd44ca42fb415e7efa5968be
abdullah1308 <abdullahrafi.1308@gmail.com>
2022-01-17 23:26:02 +0300

    Learnt basics of Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abdullah1308/BookingApp
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const conferenceTickets = 50
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+// var bookings[50] string
+// var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName string
+	lastName string
+	email string
+	numberOfTickets uint
+}
+
+// waits for the launched goroutine to finish
+// sync package has the synchronization functionality
+var wg = sync.WaitGroup()
+
+func main() {
+	
+	greetUsers()
+	
+	//for {
+		
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			
+			bookTicket(userTickets, firstName, lastName, email)
+
+			// Add() increases the number of goroutines to wait for
+			wg.Add(1)
+			go sendTicket(userTickets, firstName, lastName, email)
+
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				//break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Entered email doesn't contain '@' sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets entered is invalid")
+			}
+		}
+		
+	//}
+	// Blocks until the wait group counter is 0.
+	wg.Wait()
+}
+
+func greetUsers()  {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint){
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	// getting the name from the user
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName, lastName, email string)  {
+	remainingTickets = remainingTickets - userTickets
+
+	// create a map for a user
+	// var userData = make(map[string]string)
+	// userData["firstName"] = firstName
+	// userData["lastName"] = lastName
+	// userData["email"] = email
+	// userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	var userData = UserData {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		numberOfTickets: userTickets,
+	}
+	
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, userData)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName, lastName, email string)  {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("###################")
+	fmt.Printf("Sending ticket:\n%v\nto email address %v\n", ticket, email)
+	fmt.Println("###################")
+	// Decrements the wait group counter by one to indicate that the goroutine has finished executing.
+	wg.Done()
+}
